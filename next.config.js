3f096d251ad07c13dcb729ce6123eb41d2775b51
harsh9539 <goyalh862@gmail.com>
2023-07-14 11:33:18 +0530

    sidebar logout work done

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental:{
+        appDir:true,
+        swcPlugins:[
+            ["next-superjson-plugin",{}]
+        ]
+    },
+    images:{
+        domains:[
+            "res.cloundinary.com",
+            "avatars.githubusercontent.com",
+            "lh3.googleusercontent.com",
+            "upload.wikimedia.org"
+        ]
+    }
+}
 
 module.exports = nextConfig
